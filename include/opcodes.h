51b7bd3f49affcb2be02afeae234b2d0fc235c02
FoxGlow <hugolinma@gmail.com>
2020-07-17 17:47:20 +0200

    7/35 opcodes done

---
+++ include/opcodes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../include/chip8.h"
+
+extern void exe_cls(Chip8* chip8);
+extern void exe_ret(Chip8* chip8);
+extern void exe_jp_addr(Chip8* chip8);
+extern void exe_call_addr(Chip8* chip8);
+extern void exe_se_vx_byte(Chip8* chip8);
+extern void exe_sne_vx_byte(Chip8* chip8);
+extern void exe_se_vx_vy(Chip8* chip8);
+extern void exe_ld_vx_byte(Chip8* chip8);
---
+++ src/opcodes.c
@@ -0,0 +1,47 @@
+#include "../include/opcodes.h"
+
+void exe_cls(Chip8* chip8)
+{
+
+}
+
+void exe_ret(Chip8* chip8)
+{
+    chip8->PC = chip8->stack[chip8->SP];
+    --chip8->SP;
+}
+
+void exe_jp_addr(Chip8* chip8)
+{
+    chip8->PC = chip8->opcode & 0x0FFF;
+}
+
+void exe_call_addr(Chip8* chip8)
+{
+    ++chip8->SP;
+    chip8->stack[chip8->SP] = chip8->PC;
+    chip8->PC = chip8->opcode & 0x0FFF;
+}
+
+void exe_se_vx_byte(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] == chip8->opcode & 0x00FF)
+        chip8->PC += 2;
+}
+
+void exe_sne_vx_byte(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] != chip8->opcode & 0x00FF)
+        chip8->PC += 2;
+}
+
+void exe_se_vx_vy(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] == chip8->V[(chip8->opcode >> 4) & 0x000F])
+        chip8->PC += 2;
+}
+
+void exe_ld_vx_byte(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] = chip8->opcode & 0x00FF;
+}
