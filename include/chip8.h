76118ccd76b700f104b14ccfd46e78ed75829ee1
FoxGlow <hugolinma@gmail.com>
2020-11-18 10:48:54 +0100

    All opcodes

--- include/chip8.h
+++ include/chip8.h
@@ -5,43 +5,42 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include "../include/opcodes.h"
-
 #ifndef MEM_SIZE
     #define MEM_SIZE 0x1000
 #endif
-#ifndef REG_COUNT
-    #define REG_COUNT 0x10
+#ifndef REG_NB
+    #define REG_NB 0x10
 #endif
 #ifndef STACK_SIZE
     #define STACK_SIZE 0x10
 #endif
-#ifndef KEYS_COUNT
-    #define KEYS_COUNT 0x10
+#ifndef KEYS_NB
+    #define KEYS_NB 0x10
 #endif
-#ifndef SCREEN_SIZE
-    #define SCREEN_SIZE 0x800
+#ifndef PIX_NB
+    #define PIX_NB 0x800
 #endif
 #ifndef GAME_ADDRESS
     #define GAME_ADDRESS 0x200
 #endif
 
 typedef struct Chip8 {
-    uint8_t memory[MEM_SIZE]; // 4096 bytes of memory
-    uint8_t V[REG_COUNT]; // 16 registries
-    unsigned short opcode; // current opcode
-    unsigned short I; // index register
-    unsigned short PC; // program counter
-    unsigned short stack[STACK_SIZE]; // used to remember current location
-    unsigned short SP; // stack pointer
-    uint8_t sound_timer; // used for sound effects
-    uint8_t delay_timer; // used for timing the events of games
-    uint8_t key[KEYS_COUNT]; // current state of the key
-    uint8_t gfx[SCREEN_SIZE]; // black and white screen of 32*64 pixels
+    uint8_t memory[MEM_SIZE];
+    uint8_t V[REG_NB];
+    uint16_t opcode;
+    uint16_t I;
+    uint16_t PC;
+    uint16_t stack[STACK_SIZE];
+    uint16_t SP;
+    uint8_t sound_timer;
+    uint8_t delay_timer;
+    uint8_t key[KEYS_NB];
+    uint8_t gfx[PIX_NB];
     bool draw_flag;
 } Chip8;
 
 extern Chip8* Chip8_factory();
 extern void Chip8_loadROM(Chip8* chip8, const char* rom_path);
 extern void Chip8_emulate_cycle(Chip8* chip8);
+extern void Chip8_tick(Chip8* chip8);
 extern void Chip8_destructor(Chip8* chip8);
--- src/chip8.c
+++ src/chip8.c
@@ -35,10 +35,10 @@ Chip8* Chip8_factory()
     chip8->sound_timer = 0;
     chip8->draw_flag = false;
     memset(chip8->memory, 0, sizeof(uint8_t) * MEM_SIZE);
-    memset(chip8->V, 0, sizeof(uint8_t) * REG_COUNT);
+    memset(chip8->V, 0, sizeof(uint8_t) * REG_NB);
     memset(chip8->stack, 0, sizeof(uint8_t) * STACK_SIZE);
-    memset(chip8->key, 0, sizeof(uint8_t) * KEYS_COUNT);
-    memset(chip8->gfx, 0, sizeof(uint8_t) * SCREEN_SIZE);
+    memset(chip8->key, 0, sizeof(uint8_t) * KEYS_NB);
+    memset(chip8->gfx, 0, sizeof(uint8_t) * PIX_NB);
     for (int i = 0; i < 80; i++)
         chip8->memory[i] = FONTSET[i];
     return chip8;
@@ -56,6 +56,265 @@ void Chip8_loadROM(Chip8* chip8, const char* rom_path)
     fclose(rom);
 }
 
+void exe_cls(Chip8* chip8)
+{
+    memset(chip8->gfx, 0, sizeof(uint8_t) * PIX_NB);
+    chip8->draw_flag = true;
+    chip8->PC += 2;
+}
+
+void exe_ret(Chip8* chip8)
+{
+    chip8->PC = chip8->stack[chip8->SP--];
+    chip8->PC += 2;
+}
+
+void exe_jp_addr(Chip8* chip8)
+{
+    chip8->PC = chip8->opcode & 0x0FFF;
+}
+
+void exe_call_addr(Chip8* chip8)
+{
+    ++chip8->SP;
+    chip8->stack[chip8->SP] = chip8->PC;
+    chip8->PC = chip8->opcode & 0x0FFF;
+}
+
+void exe_se_vx_byte(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] == (chip8->opcode & 0x00FF))
+        chip8->PC += 4;
+    else
+        chip8->PC += 2;
+}
+
+void exe_sne_vx_byte(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] != (chip8->opcode & 0x00FF))
+        chip8->PC += 4;
+    else
+        chip8->PC += 2;
+}
+
+void exe_se_vx_vy(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] == chip8->V[(chip8->opcode >> 4) & 0x000F])
+        chip8->PC += 4;
+    else
+        chip8->PC += 2;
+}
+
+void exe_ld_vx_byte(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] = chip8->opcode & 0x00FF;
+    chip8->PC += 2;
+}
+
+void exe_add_vx_byte(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] += chip8->opcode & 0x00FF;
+    chip8->PC += 2;
+}
+
+void exe_ld_vx_vy(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] = chip8->V[(chip8->opcode >> 4) & 0x000F];
+}
+
+void exe_or_vx_vy(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] |= chip8->V[(chip8->opcode >> 4) & 0x000F];
+}
+
+void exe_and_vx_vy(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] &= chip8->V[(chip8->opcode >> 4) & 0x000F];
+}
+
+void exe_xor_vx_vy(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] ^= chip8->V[(chip8->opcode >> 4) & 0x000F];
+}
+
+void exe_add_vx_vy(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] + chip8->V[(chip8->opcode >> 4) & 0x000F] > 0xFF)
+        chip8->V[0xF] = 1;
+    else
+        chip8->V[0xF] = 0;
+    chip8->V[(chip8->opcode >> 8) & 0x000F] += chip8->V[(chip8->opcode >> 4) & 0x000F];
+}
+
+void exe_sub_vx_vy(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] > chip8->V[(chip8->opcode >> 4) & 0x000F])
+        chip8->V[0xF] = 1;
+    else
+        chip8->V[0xF] = 0;
+    chip8->V[(chip8->opcode >> 8) & 0x000F] -= chip8->V[(chip8->opcode >> 4) & 0x000F];
+}
+
+void exe_shr_vx_vy(Chip8* chip8)
+{
+    chip8->V[0xF] = chip8->V[(chip8->opcode >> 8) & 0x0001];
+    chip8->V[(chip8->opcode >> 8) & 0x000F] >>= 1;
+}
+
+void exe_subn_vx_vy(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 4) & 0x000F] > chip8->V[(chip8->opcode >> 8) & 0x000F])
+        chip8->V[0xF] = 1;
+    else
+        chip8->V[0xF] = 0;
+    chip8->V[(chip8->opcode >> 8) & 0x000F] =
+        chip8->V[(chip8->opcode >> 4) & 0x000F] - chip8->V[(chip8->opcode >> 8) & 0x000F];
+}
+
+void exe_shl_vx_vy(Chip8* chip8)
+{
+    chip8->V[0xF] = (chip8->V[(chip8->opcode >> 8) & 0x000F] >> 7) & 0x1;
+    chip8->V[(chip8->opcode >> 8) & 0x000F] <<= 1;
+}
+
+void exe_sne_vx_vy(Chip8* chip8)
+{
+    if (chip8->V[(chip8->opcode >> 8) & 0x000F] != chip8->V[(chip8->opcode >> 4) & 0x000F])
+        chip8->PC += 4;
+    else
+        chip8->PC += 2;
+}
+
+void exe_ld_i_addr(Chip8* chip8)
+{
+    chip8->I = chip8->opcode & 0x0FFF;
+    chip8->PC += 2;
+}
+
+void exe_jp_v0_addr(Chip8* chip8)
+{
+    chip8->PC = (chip8->opcode & 0x0FFF) + chip8->V[0];
+}
+
+void exe_rnd_vx_byte(Chip8* chip8)
+{
+    uint8_t rnd = rand() % 0x100;
+    chip8->V[(chip8->opcode >> 8) & 0x000F] = rnd & (chip8->opcode & 0x00FF);
+    chip8->PC += 2;
+}
+
+void exe_drw_vx_vy_nibble(Chip8* chip8)
+{
+    uint8_t x_pos = chip8->V[(chip8->opcode >> 8) & 0x000F];
+    uint8_t y_pos = chip8->V[(chip8->opcode >> 4) & 0x000F];
+    uint8_t height = chip8->opcode & 0x000F;
+    uint8_t pixel;
+    chip8->V[0xF] = 0;
+    for (uint8_t row = 0; row < height; row++)
+    {
+        pixel = chip8->memory[chip8->I + row];
+        for (uint8_t col = 0; col < 8; col++)
+        {
+            if ((pixel & (0x80 >> col)) != 0)
+            {
+                if (chip8->gfx[x_pos + col + ((y_pos + row) * 64)] == 1)
+                    chip8->V[0xF] = 1;
+                chip8->gfx[x_pos + col + ((y_pos + row) * 64)] ^= 1;
+            }
+        }
+    }
+    chip8->draw_flag = true;
+    chip8->PC += 2;
+}
+
+void exe_skp_vx(Chip8* chip8)
+{
+    if (chip8->key[chip8->V[(chip8->opcode >> 8) & 0x000F]])
+        chip8->PC += 4;
+    else
+        chip8->PC += 2;
+}
+
+void exe_sknp_vx(Chip8* chip8)
+{
+    if (!chip8->key[chip8->V[(chip8->opcode >> 8) & 0x000F]])
+        chip8->PC += 4;
+    else
+        chip8->PC += 2;
+}
+
+void exe_ld_vx_dt(Chip8* chip8)
+{
+    chip8->V[(chip8->opcode >> 8) & 0x000F] = chip8->delay_timer;
+    chip8->PC += 2;
+}
+
+void exe_ld_vx_k(Chip8* chip8)
+{
+    bool state = true;
+    while (true)
+    {
+        for (int i = 0; i < KEYS_NB; i++)
+        {
+            if (chip8->key[i])
+            {
+                chip8->V[(chip8->opcode >> 8) & 0x000F] = i;
+                state = false;
+            }
+        }
+    }
+    chip8->PC += 2;
+}
+
+void exe_ld_dt_vx(Chip8* chip8)
+{
+    chip8->delay_timer = chip8->V[(chip8->opcode >> 8) & 0x000F];
+    chip8->PC += 2;
+}
+
+void exe_ld_st_vx(Chip8* chip8)
+{
+    chip8->sound_timer = chip8->V[(chip8->opcode >> 8) & 0x000F];
+    chip8->PC += 2;
+}
+
+void exe_add_i_vx(Chip8* chip8)
+{
+    chip8->I += chip8->V[(chip8->opcode >> 8) & 0x000F];
+    chip8->PC += 2;
+}
+
+void exe_ld_f_vx(Chip8* chip8)
+{
+    chip8->I = chip8->V[(chip8->opcode >> 8) & 0x000F] * 5;
+    chip8->PC += 2;
+}
+
+void exe_ld_b_vx(Chip8* chip8)
+{
+    uint8_t x = (chip8->opcode >> 8) & 0x000F;
+    chip8->memory[chip8->I] = (chip8->V[x] % 1000) / 100;
+    chip8->memory[chip8->I + 1] = (chip8->V[x] % 100) / 10;
+    chip8->memory[chip8->I + 2] = chip8->V[x] % 10;
+    chip8->PC += 2;
+}
+
+void exe_ld_i_vx(Chip8* chip8)
+{
+    for (int i = 0; i <= ((chip8->opcode >> 8) & 0x000F); i++)
+        chip8->memory[chip8->I + i] = chip8->V[i];
+    chip8->I += ((chip8->opcode >> 8) & 0x000F) + 1;
+    chip8->PC += 2;
+}
+
+void exe_ld_vx_i(Chip8* chip8)
+{
+    for (int i = 0; i <= ((chip8->opcode >> 8) & 0x000F); i++)
+        chip8->V[i] = chip8->memory[chip8->I + i];
+    chip8->I += ((chip8->opcode >> 8) & 0x000F) + 1;
+    chip8->PC += 2;
+}
+
 void Chip8_emulate_cycle(Chip8* chip8)
 {
     chip8->opcode = chip8->memory[chip8->PC] << 8 | chip8->memory[chip8->PC + 1]; // concat two bytes
@@ -64,63 +323,64 @@ void Chip8_emulate_cycle(Chip8* chip8)
         case 0x0000:
             switch (chip8->opcode & 0x00FF)
             {
-                case 0x00E0: exe_cls(chip8); break; // Clears the screen
+                case 0x00E0: exe_cls(chip8); break;
                     
-                case 0x00EE: exe_ret(chip8); break; // Returns from a subroutine
+                case 0x00EE: exe_ret(chip8); break;
             }
             break;
-        case 0x1000: exe_jp_addr(chip8); break; // Jumps to address NNN
-        case 0x2000: exe_call_addr(chip8); break; // Calls a subroutine at NNN
-        case 0x3000: exe_se_vx_byte(chip8); break; // Skips the next instruction if VX == NN
-        case 0x4000: exe_sne_vx_byte(chip8); break; // Skips the next instruction if VX != NN
-        case 0x5000: exe_se_vx_vy(chip8); break; // Skips the next instruction if VX == VY
-        case 0x6000: exe_ld_vx_byte(chip8); break;// Sets VX to NN
-        case 0x7000: exe_add_vx_byte(chip8); break; // Adds NN to VX
+        case 0x1000: exe_jp_addr(chip8); break;
+        case 0x2000: exe_call_addr(chip8); break;
+        case 0x3000: exe_se_vx_byte(chip8); break;
+        case 0x4000: exe_sne_vx_byte(chip8); break;
+        case 0x5000: exe_se_vx_vy(chip8); break;
+        case 0x6000: exe_ld_vx_byte(chip8); break;
+        case 0x7000: exe_add_vx_byte(chip8); break;
         case 0x8000:
             switch (chip8->opcode & 0x000F)
             {
-                case 0x0000: exe_ld_vx_vy(chip8); break; // Sets VX to the value of VY
-                case 0x0001: exe_or_vx_vy(chip8); break; // Sets VX to VX OR VY
-                case 0x0002: exe_and_vx_vy(chip8); break; // Sets VX to VX AND VY
-                case 0x0003: exe_xor_vx_vy(chip8); break; // Sets VX to VX XOR VY
-                case 0x0004: exe_add_vx_vy(chip8); break; // Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
-                case 0x0005: exe_sub_vx_vy(chip8); break; // VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
-                case 0x0006: exe_shr_vx_vy(chip8); break; // VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
-                case 0x0007: exe_subn_vx_vy(chip8); break; // Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
-                case 0x000E: exe_shl_vx_vy(chip8); break; // Stores the most significant bit of VX in VF and then shifts VX to the left by 1
+                case 0x0000: exe_ld_vx_vy(chip8); break;
+                case 0x0001: exe_or_vx_vy(chip8); break;
+                case 0x0002: exe_and_vx_vy(chip8); break;
+                case 0x0003: exe_xor_vx_vy(chip8); break;
+                case 0x0004: exe_add_vx_vy(chip8); break;
+                case 0x0005: exe_sub_vx_vy(chip8); break;
+                case 0x0006: exe_shr_vx_vy(chip8); break;
+                case 0x0007: exe_subn_vx_vy(chip8); break;
+                case 0x000E: exe_shl_vx_vy(chip8); break;
             }
+            chip8->PC += 2;
             break;
-        case 0x9000: exe_sne_vx_vy(chip8); break; // Skips the next instruction if VX != VY
-        case 0xA000: exe_ld_i_addr(chip8); break; // Sets I to the address NNN
-        case 0xB000: exe_jp_v0_addr(chip8); break; // Jumps to the address NNN plus V0
-        case 0xC000: exe_rnd_vx_byte(chip8); break; // Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN
-        case 0xD000: exe_drw_vx_vy_nibble(chip8); break; // Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels
+        case 0x9000: exe_sne_vx_vy(chip8); break;
+        case 0xA000: exe_ld_i_addr(chip8); break;
+        case 0xB000: exe_jp_v0_addr(chip8); break;
+        case 0xC000: exe_rnd_vx_byte(chip8); break;
+        case 0xD000: exe_drw_vx_vy_nibble(chip8); break;
         case 0xE000:
             switch (chip8->opcode & 0x00FF)
                 {
-                case 0x009E: exe_skp_vx(chip8); break; // Skips the next instruction if the key stored in VX is pressed
-                case 0x00A1: exe_sknp_vx(chip8); break; // Skips the next instruction if the key stored in VX isn't pressed
+                case 0x009E: exe_skp_vx(chip8); break;
+                case 0x00A1: exe_sknp_vx(chip8); break;
             }
             break;
         case 0xF000:
             switch (chip8->opcode & 0x00FF)
             {
-                case 0x0007: exe_ld_vx_dt(chip8); break;// Sets VX to the value of the delay timer
-                case 0x000A: exe_ld_vx_k(chip8); break; // A key press is awaited, and then stored in VX
-                case 0x0015: exe_ld_dt_vx(chip8); break; // Sets the delay timer to VX
-                case 0x0018: exe_ld_st_vx(chip8); break; // Sets the sound timer to VX
-                case 0x001E: exe_add_i_vx(chip8); break; // Adds VX to I. VF is not affected
-                case 0x0029: // Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
-                    break;
-                case 0x0033:
-                    break;
-                case 0x0055:
-                    break;
-                case 0x0065:
-                    break;
+                case 0x0007: exe_ld_vx_dt(chip8); break;
+                case 0x000A: exe_ld_vx_k(chip8); break;
+                case 0x0015: exe_ld_dt_vx(chip8); break;
+                case 0x0018: exe_ld_st_vx(chip8); break;
+                case 0x001E: exe_add_i_vx(chip8); break;
+                case 0x0029: exe_ld_f_vx(chip8); break;
+                case 0x0033: exe_ld_b_vx(chip8); break;
+                case 0x0055: exe_ld_i_vx(chip8); break;
+                case 0x0065: exe_ld_vx_i(chip8); break;
             }
             break;
     }
+}
+
+void Chip8_tick(Chip8* chip8)
+{
     if (chip8->delay_timer > 0)
         --chip8->delay_timer;
     if (chip8->sound_timer > 0)
